d7531999988f16b627c10df42b8801f9f89f220d
Franco C Sanchez <francocsanchez@gmail.com>
2021-08-16 19:02:43 -0300

    Reforma de archivos

---
+++ contadorVocales.js
@@ -0,0 +1,14 @@
+/*
+Implement the function unique_in_order which takes as argument a sequence and returns 
+a list of items without any elements with the same value next to each other and preserving 
+the original order of elements.
+*/
+
+function separarString(string) {
+    let arrayString = string.split('');
+    let contarVocales;
+    let total = arrayString.filter(word => word === 'a' || word === 'e' || word === 'i' || word === 'o' || word === 'u');
+    return total.length;
+}
+
+console.log(separarString('jidoqujowqmsmlxa'));
