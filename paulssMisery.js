aa45fb5705f087f8f05f104ab4465ba342b796c1
Franco C Sanchez <francocsanchez@gmail.com>
2021-08-17 14:01:14 -0300

    Function reduce

--- paulssMisery.js
+++ paulssMisery.js
@@ -35,3 +35,11 @@ function paul(x) {
     else if (totalPoints < 40) { return 'Super happy!'; }
 
 }
+
+/*
+function paul(arr){
+  const dic = {kata: 5, 'Petes kata': 10, life: 0, eating: 1}
+  const num = arr.reduce( (a, i)=> +a + dic[i], 0)
+  return num < 40 ? 'Super happy!' : num < 70 ? 'Happy!' : num < 100 ? 'Sad!' : 'Miserable!'
+}
+*/
