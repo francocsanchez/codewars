f802fddc542509a09b8bc2173c810f01826d1882
Franco C Sanchez <francocsanchez@gmail.com>
2021-08-17 13:58:35 -0300

    Pau's Misery

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-### Ejercitacion?
+![badge-codewars](https://www.codewars.com/users/francocsanchez/badges/large)
+
+### Katas
 * [Abbreviatea Two Word Name](https://github.com/francocsanchez/codewars/blob/master/abbreviateaTwoWordName.js)
 * [Array de divisores](https://github.com/francocsanchez/codewars/blob/master/arrayDeDivisores.js)
 * [Buenas y malas ideas](https://github.com/francocsanchez/codewars/blob/master/buenasYMalasIdeas.js)
@@ -7,7 +9,6 @@
 * [Contador de positivos y suma de negativos](https://github.com/francocsanchez/codewars/blob/master/contarPositivosSumarNegativos.js)
 * [Eliminar espacios](https://github.com/francocsanchez/codewars/blob/master/eliminarEspacios.js)
 * [Java Script Array Filter](https://github.com/francocsanchez/codewars/blob/master/javaScriptArrayFilter.js)
+* [Paul's Misery](https://github.com/francocsanchez/codewars/blob/master/paulssMisery.js)
 
 _Ejercicios obtenidos del sitio de www.codewars.com_
-
-![badge-codewars](https://www.codewars.com/users/francocsanchez/badges/large)
---
+++ paulssMisery.js
@@ -0,0 +1,37 @@
+/*
+Paul is an excellent coder and sits high on the CW leaderboard. He solves kata like a 
+banshee but would also like to lead a normal life, with other activities. But he just 
+can't stop solving all the kata!!
+
+Given an array (x) you need to calculate the Paul Misery Score. The values are worth the following points:
+
+kata = 5
+Petes kata = 10
+life = 0
+eating = 1
+
+The Misery Score is the total points gained from the array. Once you have the total, return as follows:
+
+< 40 = 'Super happy!'
+< 70 >= 40 = 'Happy!'
+< 100 >= 70 = 'Sad!'
+> 100 = 'Miserable!'
+*/
+
+function paul(x) {
+    let kata = x.filter(x => x === 'kata');
+    let petesKata = x.filter(x => x === 'Petes kata');
+    let eating = x.filter(x => x === 'eating');
+
+    let kataPoints = kata.length * 5;
+    let petesKataPoints = petesKata.length * 10;
+    let eatingPonits = eating.length * 1;
+
+    let totalPoints = kataPoints + petesKataPoints + eatingPonits;
+
+    if (totalPoints > 100) { return 'Miserable!'; }
+    else if (totalPoints < 100 && totalPoints >= 70) { return 'Sad!'; }
+    else if (totalPoints < 70 && totalPoints >= 40) { return 'Happy!'; }
+    else if (totalPoints < 40) { return 'Super happy!'; }
+
+}
